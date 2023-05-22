fc6c86a02d231f3a3feec8f7dd2e58156a449df4
Sebco33 <120632248+Sebco33@users.noreply.github.com>
2023-05-22 17:29:55 +0200

    Update Cypress_Plugin.cy.js

--- cypress/e2e/Cypress_Plugin.cy.js
+++ cypress/e2e/Cypress_Plugin.cy.js
@@ -1,41 +1,8 @@
-describe("File upload and download tests", () => {
-  beforeEach(() => {
-    cy.visit("https://filebin.net/6ndjhwrwxg23l6hh");
-  });
+describe('File upload', () => { 
+        it ('uploads a file', () => { 
+        cy.visit('https://filebin.net/'); 
+        cy.get('input[type ="file"]') .attachFile('Chien.png');     
 
-  it("Upload file and download it in Zip format", () => {
-    cy.get("#fileField").attachFile("fileToUpload.jpg");
-    cy.contains("It contains 1 uploaded file").should("be.visible");
-    cy.contains("Download files").click();
-    cy.contains("Zip")
-      .invoke("attr", "href")
-      .then((downloadLink) => {
-        const absulteLink = "https://filebin.net/" + downloadLink;
-        cy.log(absulteLink);
-        cy.downloadFile(
-          absulteLink,
-          "mydownloads/zipFiles",
-          "downloadedFromCypress.zip"
-        );
-        cy.readFile("mydownloads/zipFiles/downloadedFromCypress.zip");
-      });
-  });
+   }); 
 
-  it("Upload file and download it in Tar format", () => {
-    cy.get("#fileField").attachFile("fileToUpload.jpg");
-    cy.contains("It contains 1 uploaded file").should("be.visible");
-    cy.contains("Download files").click();
-    cy.contains("Tar")
-      .invoke("attr", "href")
-      .then((downloadLink) => {
-        const absulteLink = "https://filebin.net/" + downloadLink;
-        cy.log(downloadLink);
-        cy.downloadFile(
-          absulteLink,
-          "mydownloads/tarFiles",
-          "downloadedFromCypress.tar"
-        );
-        cy.readFile("mydownloads/tarFiles/downloadedFromCypress.tar");
-      });
-  });
 });

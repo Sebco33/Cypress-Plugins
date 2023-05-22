8e8de738f3e09a50abd95f028b3fb0d0a067034d
Sebco33 <120632248+Sebco33@users.noreply.github.com>
2023-05-22 17:31:02 +0200

    Update Cypress_Plugin.cy.js

--- cypress/e2e/Cypress_Plugin.cy.js
+++ cypress/e2e/Cypress_Plugin.cy.js
@@ -1,7 +1,7 @@
 describe('File upload', () => { 
         it ('uploads a file', () => { 
         cy.visit('https://filebin.net/'); 
-        cy.get('input[type ="file"]') .attachFile('Chien.png');     
+        cy.get('input[type ="file"]').attachFile('Chien.png');     
 
    }); 
 
